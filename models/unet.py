30311810c98429e72dc884919c80f861522d35a6
Aster <2877014583@qq.com>
2025-09-19 02:05:12 +0800

    Update unet.py

--- models/unet.py
+++ models/unet.py
@@ -2,17 +2,20 @@ import torch
 import torch.nn as nn
 
 
-class DoubleConv(nn.Module):
+class Block(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(DoubleConv, self).__init__()
+        super(Block, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
-            nn.InstanceNorm2d(out_channels), nn.ReLU(inplace=True),
+            nn.InstanceNorm2d(out_channels), 
+            nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
-            nn.InstanceNorm2d(out_channels), nn.ReLU(inplace=True),
+            nn.InstanceNorm2d(out_channels), 
+            nn.ReLU(inplace=True),
         )
 
-    def forward(self, x): return self.conv(x)
+    def forward(self, x): 
+        return self.conv(x)
 
 
 class UNet(nn.Module):
@@ -23,14 +26,14 @@ class UNet(nn.Module):
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         for feature in features:
-            self.downs.append(DoubleConv(in_channels, feature))
+            self.downs.append(Block(in_channels, feature))
             in_channels = feature
 
-        self.bottleneck = DoubleConv(features[-1], features[-1] * 2)
+        self.bottleneck = Block(features[-1], features[-1] * 2)
 
         for feature in reversed(features):
             self.ups.append(nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2))
-            self.ups.append(DoubleConv(feature * 2, feature))
+            self.ups.append(Block(feature * 2, feature))
 
         self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
 
@@ -50,5 +53,6 @@ class UNet(nn.Module):
             skip_connection = skip_connections[idx // 2]
             concat_skip = torch.cat((skip_connection, x), dim=1)
             x = self.ups[idx + 1](concat_skip)
+
 
         return self.final_conv(x) + s
